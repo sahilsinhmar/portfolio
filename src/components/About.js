571f8b136d4b03be80e337a7ab26416b9fa77a2f
sahilsinhmar <sahilsinhmar07@gmail.com>
2023-09-05 12:46:36 +0530

    fix

--- src/components/About.js
+++ src/components/About.js
@@ -12,23 +12,23 @@ export const About = () => {
       <div className="flex flex-col items-center px-4 gap-6 font-Poppins font-semibold sm:text-xl text-[#3a4151] dark:text-white">
         <p className="text-3xl">About Me</p>
         <p>
-          Hi, I'm Sahil Sinhmar. I'm a Full-Stack web developer from India. I
+          {` Hi, I'm Sahil Sinhmar. I'm a Full-Stack web developer from India. I
           enjoy seeking out creative solutions to complex problems. It was back
           in late 2021 when I got interested in Web Development and decided to
           learn HTML & CSS. Fast forward to today, I learned various web
           technologies and their best practices and built several fun and
-          exciting projects.
+          exciting projects.`}
         </p>
         <p>
-          I love programming because it is so practical, and I can solve
+          {`I love programming because it is so practical, and I can solve
           real-world challenges with it. For me, programming is a tool to
           express myself in a creative way. Plus, there is so much to explore
-          that always keeps me excited!
+          that always keeps me excited!`}
         </p>
         <p>
-          In my free time, I get inspiration from other people's work and try
+          {` In my free time, I get inspiration from other people's work and try
           out new technologies to build side projects. Apart from that, I also
-          love sports and reading books sometimes.
+          love sports and reading books sometimes.`}
         </p>
         <div className="sm:flex-row flex flex-col gap-2">
           <Link
